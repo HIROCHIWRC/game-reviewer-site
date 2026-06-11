eea6583ee7fb903e4169969237430302534224f2
HIROCHIWRC <pavelsubaru2006@gmail.com>
2026-06-11 22:52:08 +0300

    root package.json with server deps + start script

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const adminRoutes = require('./routes/admin');
 const db = require('./db');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '10kb' }));
